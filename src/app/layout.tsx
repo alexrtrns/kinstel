e62595717862bb07a2a0fc44229b8786954cc4a3
Alex Mercer. <nerodantertrns@gmail.com>
2025-09-28 09:12:01 +0000

    use dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,7 +85,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth dark">
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable, fontSerif.variable)}>
         {children}
         <Toaster />
